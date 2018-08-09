917e3f4284dd9908772f86e180fb5ef284955ef2
Ella <ella@mba72-gkjv.local>
2018-08-08 21:49:47 -0400

    Add description to list item

--- client/src/components/ListItem/ListItem.js
+++ client/src/components/ListItem/ListItem.js
@@ -27,39 +27,46 @@ class ListItem extends React.Component {
           <div className="name">{name}</div>
         </div>
         <div className="info-container">
-          <div class="cuisine">{cuisine}</div>
-          <div class="otherInfo">
-            <div className="price">{priceRange}</div>
-            <StarRatingComponent
-              name="rate2"
-              editing={false}
-              starCount={5}
-              renderStarIcon={(index, value, name, id) => {
-                const isColored = index < value;
-                return (
-                  <MaterialIcon
-                    icon={isColored ? "star" : "star_border"}
-                    color="red"
-                    size={16}
-                  />
-                );
-              }}
-              renderStarIconHalf={() => (
-                <MaterialIcon icon="star_half" color="red" size={16} />
-              )}
-              value={yelpRating}
-            />
-            <div className="bld-container">
-              <div className={`meal-time brunch${brunch ? " filled" : ""}`}>
-                B
-              </div>
-              <div className={`meal-time lunch${lunch ? " filled" : ""}`}>
-                L
-              </div>
-              <div className={`meal-time dinner${dinner ? " filled" : ""}`}>
-                D
+          <div>
+            <div class="description">{description}</div>
+            <div class="otherInfo">
+              <div className="price">{priceRange}</div>
+              <StarRatingComponent
+                name="rate2"
+                editing={false}
+                starCount={5}
+                renderStarIcon={(index, value, name, id) => {
+                  const isColored = index < value;
+                  return (
+                    <MaterialIcon
+                      icon={isColored ? "star" : "star_border"}
+                      color="red"
+                      size={16}
+                    />
+                  );
+                }}
+                renderStarIconHalf={() => (
+                  <MaterialIcon icon="star_half" color="red" size={16} />
+                )}
+                value={yelpRating}
+              />
+              <div className="bld-container">
+                <div className={`meal-time brunch${brunch ? " filled" : ""}`}>
+                  B
+                </div>
+                <div className={`meal-time lunch${lunch ? " filled" : ""}`}>
+                  L
+                </div>
+                <div className={`meal-time dinner${dinner ? " filled" : ""}`}>
+                  D
+                </div>
               </div>
             </div>
+          </div>
+          <div class="cuisine header">
+            {cuisine.map(c => {
+              return <div className={`sort`}> {c} </div>;
+            })}
           </div>
         </div>
       </div>
